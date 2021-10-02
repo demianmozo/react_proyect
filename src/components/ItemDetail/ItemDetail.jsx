1a523cf05e437d30e82fbd9b35b2847323ad594d
demianmozo <demianmozo@gmail.com>
2021-10-02 13:38:02 -0300

    agregando estilos

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,21 +4,31 @@ import React from "react";
 import ItemCount from '../ItemCount/ItemCount';
 //external components
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 //functional component
 
 function ItemDetail({ title, price, stock, img, desc }) {
   return (
-      <div>
-        <div>
-          <img src={`../assets/products/${img}`} alt={desc} />
-        </div>
-        <div>
-        <h3>{title}</h3>
-        <p>{desc}</p>
-        <p><b>${price}</b></p>
-        <p>Quedan {stock} unidades</p>
-        <ItemCount initial={1} stock={stock} title={title} />
+      <div className="item-detail">
+        <div className="item-detail-header">
+          <div className="item-detail-carousel">
+            <img src={`../assets/products/${img}`} alt={desc} />
+            <img src={`../assets/products/${img}`} alt={desc} />
+            <img src={`../assets/products/${img}`} alt={desc} />
+            <img src={`../assets/products/${img}`} alt={desc} />
+          </div>
+          <div className='item-detail-header-img'>
+              <img src={`../assets/products/${img}`} alt={desc} />
+            </div>
+          </div>
+        <div className='item-detail-container'>
+          <h3>{title}</h3>
+          <p>{desc}</p>
+          <p><b>${price}</b></p>
+          <p>Quedan {stock} unidades</p>
+          <ItemCount initial={1} stock={stock} title={title} />
+          <Link to={'/'}><Button color='inherit' variant="outlined">Volver</Button></Link>
         </div>
       </div>
   );
